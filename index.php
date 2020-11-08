ec1cea69b8e5080c3503587ccf77f4fd081037fc
elliomanjo <74146296+elliomanjo@users.noreply.github.com>
2020-11-08 22:48:20 +0330

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,1 @@
 This is a test for github. we're trying to push and pull the project files to git hub repository
-=======
-This is a test for github. we're trying to push and pull the project files and assets
->>>>>>> 956639a4eafefbea5cb819d55d1f9d8e37b9aa00
